ddf9de8889e10233b29ca9042fa304f90db3cb47
Aaryan Sah <157813384+teckyaaryan@users.noreply.github.com>
2026-06-09 20:28:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
-API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
+API_ID = int(environ.get('API_ID', '32541562')) # API ID from my.telegram.org
+API_HASH = environ.get('API_HASH', 'e37e4432298d5a5eb4a6e32c18804283')  # API Hash from my.telegram.org
+BOT_TOKEN = environ.get('BOT_TOKEN', "8968204021:AAHSu4tHbuTkHNK157XWd_Vf6KwhDWxqtiU")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
